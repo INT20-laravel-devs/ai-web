c6f9a4de1cee72421806c52b0eb35572caba258b
Ihor <ihor.n@develops.today>
2025-03-01 16:59:43 +0200

    feat: added dropdown for themes

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,7 +2,6 @@ import { NuqsAdapter } from "nuqs/adapters/react";
 import { type PropsWithChildren } from "react";
 
 import AuthProvider from "@/providers/auth-provider";
-
 const Providers = ({ children }: PropsWithChildren) => {
   return (
     <AuthProvider>
